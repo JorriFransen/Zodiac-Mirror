148e1c6b9064c60d19c1fbb7aa9e94b7f81589bf
jorrifransen@gmail.com <jorrifransen@gmail.com@noemail.net>
2019-03-16 16:49:45 +0000

    Dyncall library.
    FossilOrigin-Name: a5a5332d82ac8c79ad8e0991f98ad0e7b5986c89ce6f26f782d2cddc3d670b20

--- create_linux_build.sh
+++ create_linux_build.sh
@@ -1,7 +1,12 @@
 #! /usr/bin/env bash
 
+pushd lib
+    unzip dyncall-1.0.zip
+popd
+
 mkdir -p build
 
 pushd build
     cmake .. -G "Ninja" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 popd
+
