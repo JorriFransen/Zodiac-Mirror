7d960f023a5dfd9cca85e39f7e9b15267468de7d
jorrifransen@gmail.com <jorrifransen@gmail.com@noemail.net>
2019-05-16 12:42:41 +0000

    Creating a seperate release built with cmake on linux.
    FossilOrigin-Name: 75b5cfc448d931bd185e9144e7884a2811f22b3779bf5f64891799b3abcd2162

--- create_linux_build.sh
+++ create_linux_build.sh
@@ -8,5 +8,9 @@ mkdir -p build
 
 pushd build
     cmake .. -G "Ninja" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+    mkdir -p release
+    pushd release
+    cmake ../.. -G "Ninja" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 popd
 
